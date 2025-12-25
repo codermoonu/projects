9fa257bff45d384a9adb69df917055b7e74e224c
Puja Dey <pdey56189@gmail.com>
2025-12-25 21:11:08 +0530

    python mini projects
    tried something new with Exa , handled api, made some gifs

---
+++ python/python/exa/main.py
@@ -0,0 +1,16 @@
+from exa_py import Exa
+exa = Exa('YOUR_API_KEY')
+
+query = input('Search here: ')
+response = exa.search(
+    query,
+    num_results = 5,
+    type='keyword',
+    include_domains=['https://www.tiktok.com'],
+)
+
+for result in response.results: 
+    print(f'Title: {result.title}')
+    print(f'URL: {result.url}')
+    print()
+
---
+++ python/python/image/imagegif2.py
@@ -0,0 +1,10 @@
+import imageio.v3 as iio
+filenames = ['chicklet1.png', 'chicklet2.png', 'chicklet3.png', 'chicklet4.png']
+images=[]
+
+for filename in filenames :
+    images.append(iio.imread(filename))
+
+iio.imwrite('chicklet.gif', images, duration=400, loop=0)
+    
+    
